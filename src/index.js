cca8f3fb9400049b374d6011d6b73d705f821d80
Woo Song <ws392@drexel.edu>
2020-05-24 00:30:27 -0400

    add nav to export config file

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ export * from "./components/Button";
 export * from "./components/Card";
 export * from "./components/Grid";
 export * from "./components/TwoColumnHero";
+export * from "./components/NavigationBar";
